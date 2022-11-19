254626d29c09121f8456908f863ddfcc2e2399b0
Anxhelo21 <101625586+Anxhelo21@users.noreply.github.com>
2022-11-19 11:52:18 +0100

    Delete AnotherOne directory

--- AnotherOne/script.js
+++
@@ -1,4 +0,0 @@
-function toCelsius(f){
-    f = parseFloat(f);
-    document.getElementById("outputCelsius").innerHTML=(f-32)*5/9;
-}
--- AnotherOne/testing.js
+++
@@ -1,4 +0,0 @@
-function to_Celsius(){
-    C=(5/9)*(document.getElementById("f").value-32);
-    alert("Celsius: "+C);
-}
